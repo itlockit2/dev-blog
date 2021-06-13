ade34c5b6bd615a1267ef5f878edb8c2746e308c
songhoyun <itlockit@gmail.com>
2021-06-14 01:32:56 +0900

    add gatsby-plugin-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-DBXVLDD9QP"
+      }
+    },
+    {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png"
